b070c878b6303249c0e6a5459d4034a01e8abfef
lippzhang <489435760@qq.com>
2023-05-09 11:38:56 +0800

    feat: 使用迭代器改进程序

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
     // 对 build 返回的 `Result` 进行处理
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
